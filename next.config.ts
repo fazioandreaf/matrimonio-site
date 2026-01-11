84e1efa67db7a22669cdb62465b25050464fc237
Andrea Fazio <andrea.fazio@thelevelgroup.com>
2026-01-11 10:41:46 +0100

    Aggiunta di configurazioni

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: new URL(process.env.R2_PUBLIC_URL || 'https://pub-118c09941ed84b2aa0d3315468271eb9.r2.dev').hostname,
-        port: '',
-        pathname: '/**',
-      },
-    ],
-  },
+	images: {
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: new URL(
+					process.env.R2_PUBLIC_URL ||
+						"https://pub-118c09941ed84b2aa0d3315468271eb9.r2.dev"
+				).hostname,
+				port: "",
+				pathname: "/**",
+			},
+		],
+	},
 };
 
 export default nextConfig;
